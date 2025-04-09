108fa8e6f869f776c886343f96cf37c373627547
DmitriyBabii <v.babiy75@gmail.com>
2025-04-09 19:15:53 +0300

    import kafka config

--- main/src/main/java/com/snowragecat/main/CrmApplication.java
+++ main/src/main/java/com/snowragecat/main/CrmApplication.java
@@ -1,13 +1,16 @@
 package com.snowragecat.main;
 
+import com.snowragecat.shared.configs.KafkaConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+@Import(KafkaConfig.class)
 @SpringBootApplication
 public class CrmApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CrmApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CrmApplication.class, args);
+    }
 
 }
--- openai/src/main/java/com/snowragecat/openai/CrmAiApplication.java
+++ openai/src/main/java/com/snowragecat/openai/CrmAiApplication.java
@@ -1,8 +1,11 @@
 package com.snowragecat.openai;
 
+import com.snowragecat.shared.configs.KafkaConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+@Import(KafkaConfig.class)
 @SpringBootApplication
 public class CrmAiApplication {
 
